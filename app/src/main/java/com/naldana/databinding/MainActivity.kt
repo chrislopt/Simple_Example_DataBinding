e472a4e1b2acc5deafdbb9068cb0fe08b2a947ef
Nestor <nestor.aldana1@gmail.com>
2020-04-23 14:52:04 -0600

    Base Code

--- app/src/main/java/com/naldana/databinding/MainActivity.kt
+++ app/src/main/java/com/naldana/databinding/MainActivity.kt
@@ -2,41 +2,16 @@ package com.naldana.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import androidx.databinding.DataBindingUtil
-import com.naldana.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     var inCounter = Counter("In", 0)
     var outCounter = Counter("Out", 0)
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        binding.inCounter = inCounter
-        binding.outCounter = outCounter
-
-        addCounterActions()
-
+        setContentView(R.layout.activity_main)
     }
 
-    private fun addCounterActions() {
-
-        binding.apply {
-            buttonAddOut.setOnClickListener {
-                this@MainActivity.outCounter.count++
-                invalidateAll()
-            }
-            buttonAddIn.setOnClickListener {
-                this@MainActivity.inCounter.count++
-                invalidateAll()
-            }
-        }
-    }
 
 }
